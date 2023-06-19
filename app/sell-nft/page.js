deaabc95381977c2708bc618efe1607b185dbaad
robitu <robertoiturralde2009@hotmail.com>
2023-06-19 15:38:45 -0600

    Added tab title

--- app/sell-nft/page.js
+++ app/sell-nft/page.js
@@ -1,8 +1,15 @@
 import Image from "next/image"
+import Head from "next/head"
+import { Metadata } from "next"
+
+export const metadata = {
+    title: "NFT Marketplace",
+    description: "NFT",
+}
 
 export default function sellNft() {
     return (
-        <main>
+        <main className="flex min-h-screen flex-col justify-between p-24">
             <h1>Sell NFT</h1>
         </main>
     )
