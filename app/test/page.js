c56eb54f24b97309f03b0d9994a6c3b6923cca43
robitu <robertoiturralde2009@hotmail.com>
2023-07-12 15:15:15 -0600

    Created new pages and changed tab titles

--- app/test/page.js
+++ app/test/page.js
@@ -3,14 +3,14 @@ import Head from "next/head"
 import { Metadata } from "next"
 
 export const metadata = {
-    title: "Sell NFT",
-    description: "NFT selling page",
+    title: "Test Page",
+    description: "test page",
 }
 
-export default function sellNft() {
+export default function test() {
     return (
         <main className="flex min-h-screen flex-col justify-between p-24">
-            <h1>Sell NFT</h1>
+            <h1>OHAIUUUUUUUU</h1>
         </main>
     )
 }
